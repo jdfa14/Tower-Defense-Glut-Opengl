b7826df912a43ccc178f2cd5a07cb640d82040e9
David Quilla <davichoq@gmail.com>
2014-11-04 22:49:53 -0600

    Incomplete Sprites.h
    This .h is incomplete

---
+++ Proyecto_Graficas/Sprites.h
@@ -0,0 +1,28 @@
+
+class Sprites {
+	Vector<Images> vectorImagenes;
+	double tiempoTransicion;
+	public:
+	void setPositions(double, double, double);
+	void setSizes(double, double, double);
+	bool addSprites(string);
+	bool deleteSprites();
+	void draw(double);
+};
+
+void Sprites::setPositions(double x, double y, double z) {
+
+}
+void Sprites::setSizes(double x, double y, double y) {
+}
+bool Sprites::addSprites(string path) {
+
+	return true;
+}
+bool Sprites::deleteSprites() {
+	
+	return true;
+}
+void Sprites::draw(double tiempoTranscurrido) {
+
+}
