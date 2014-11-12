d8e183d6c252be3ce36c000f091c88e50f247359
David Quilla <davichoq@gmail.com>
2014-11-11 21:57:19 -0600

    .

--- Proyecto_Graficas/Sprites.h
+++ Proyecto_Graficas/Sprites.h
@@ -1,12 +1,15 @@
-
+#include "imageloader.h"
 class Sprites {
 	Vector<Images> vectorImagenes;
 	double tiempoTransicion;
+	int cantSprites;
+	static GLuint texName[90];
+	
 	public:
 	Sprites();
 	void setPositions(double, double, double);
 	void setSizes(double, double, double);
-	bool addSprites(string);
+	bool addSprites(string, int);
 	bool deleteSprites();
 	void draw(double);
 };
@@ -18,8 +21,25 @@ void Sprites::setPositions(double x, double y, double z) {
 }
 void Sprites::setSizes(double x, double y, double y) {
 }
-bool Sprites::addSprites(string path) {
+bool Sprites::addSprites(string path, int i) {
 
+   glBindTexture(GL_TEXTURE_2D, texName[k]); //Tell OpenGL which texture to edit
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    //Map the image to the texture
+    glTexImage2D(GL_TEXTURE_2D,                 //Always GL_TEXTURE_2D
+                 0,                             //0 for now
+                 GL_RGB,                        //Format OpenGL uses for image
+                 image->width, image->height,   //Width and height
+                 0,                             //The border of the image
+                 GL_RGB,                        //GL_RGB, because pixels are stored in RGB format
+                 GL_UNSIGNED_BYTE,              //GL_UNSIGNED_BYTE, because pixels are stored
+                                                //as unsigned numbers
+                 image->pixels);                //The actual pixel data
+	
+	
+	
+	
 	return true;
 }
 bool Sprites::deleteSprites() {
