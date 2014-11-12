f926a64f958c06874fc2eccc7ea73a52c3fc0a87
David Quilla <davichoq@gmail.com>
2014-11-12 13:23:37 -0600

    Tower Stuff
    Not completed

--- Proyecto_Graficas/Tower.cpp
+++ Proyecto_Graficas/Tower.cpp
@@ -1,1 +1,6 @@
 #include "Tower.h"
+
+void Tower::setPosition(int a, int b) {
+    posicion.x = a;
+    posicion.y = b;
+}
--- Proyecto_Graficas/Tower.h
+++ Proyecto_Graficas/Tower.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "StaticObject.h"
+
+typedef struct {
+    int x;
+    int y;
+} vectorPos;
+
 class Tower : public StaticObject
 {
+    private:
+        vectorPos posicion;
     public:
-
-        Tower()
-        {
-
-        }
+        Tower();
+        void setPosition(int, int);
+        void getPosition(int, int);
 };
 
