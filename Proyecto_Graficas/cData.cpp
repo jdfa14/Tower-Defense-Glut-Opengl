e5c58ee45c5f3871365380eb68b64a303cb533f4
David Quilla <davichoq@gmail.com>
2014-11-13 17:53:58 -0600

    cData Tower loading
    Put the code to load towers

--- Proyecto_Graficas/cData.cpp
+++ Proyecto_Graficas/cData.cpp
@@ -35,5 +35,21 @@ bool cData::Load()
 	res = LoadImage(IMG_BGMAIN, path + "menu.png", GL_RGBA); if (!res) return false;
 	res = LoadImage(IMG_BGCREDITS, path + "credits.png", GL_RGBA); if (!res) return false;
 
+	// loading towers
+	path = "Images/towersPNG";
+	// Pills
+	res = LoadImage(IMG_TOWER1NORMAL, path + "tower1Normal.png", GL_RGBA); if (!res) return false;
+	res = LoadImage(IMG_TOWER1UPGRADED, path + "tower1Upgraded.png", GL_RGBA); if (!res) return false;
+	res = LoadImage(IMG_TOWER1SUPER, path + "tower1Super.png", GL_RGBA); if (!res) return false;
+	// White cell
+    res = LoadImage(IMG_TOWER2NORMAL, path + "tower2Normal.png", GL_RGBA); if (!res) return false;
+	res = LoadImage(IMG_TOWER2UPGRADED, path + "tower2Upgraded.png", GL_RGBA); if (!res) return false;
+	res = LoadImage(IMG_TOWER2SUPER, path + "tower2Super.png", GL_RGBA); if (!res) return false;
+	// Yellow cell
+    res = LoadImage(IMG_TOWER3NORMAL, path + "tower3Normal.png", GL_RGBA); if (!res) return false;
+	res = LoadImage(IMG_TOWER3UPGRADED, path + "tower3Upgraded.png", GL_RGBA); if (!res) return false;
+	res = LoadImage(IMG_TOWER3SUPER, path + "tower3Super.png", GL_RGBA); if (!res) return false;
+
+
 	return true;
 }
